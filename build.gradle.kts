1cd0879e83d8717704c6f83704b781dd86238fdd
found-cake <skymin468@gmail.com>
2023-12-10 01:34:10 +0900

    java 버전 명시

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,12 @@ plugins {
 group = "io.github.java_2048"
 version = "1.0-SNAPSHOT"
 
+java {
+    toolchain{
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 repositories {
     mavenCentral()
 }
